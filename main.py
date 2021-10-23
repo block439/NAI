c091f7ccf410907c47588b2fcb487ed1789b25c7
block439 <olekreiter@gmail.com>
2021-10-23 09:05:22 +0200

    Added Engine

--- main.py
+++ main.py
@@ -1,63 +1,105 @@
 """
-    Zmierz się ze sztuczną intelignecją w szachach :)
-    Zasady gry w szachy: https://pl.wikipedia.org/wiki/Zasady_gry_w_szachy
-    W celu dokonania ruchu użyj notacji algebraicznej wpisując do konsoli swój ruch.
-    Na przykład: ruch pionkiem z pola a2 na pole a3 będzie wyglądać następująco a2a3.
+Chess rules [PL]: https://www.ikonka.com.pl/kontext/file/2236/KX8940.pdf
 
-    W celu możliwości uruchomienia gry należy pobrać i zainstalować bibliotekę easyAi i chess.
-    W konsoli użyj komendy 'pip install -r /path/to/requirements.txt' pozwoli na automatyczną instalację potrzebnych bibliotek
+Authors:
+Reiter, Aleksander <https://github.com/block439>
+Dziadowiec, Mieszko <https://github.com/mieshki>
 
-    Autorzy: Mieszko Dziadowiec, Aleksander Reiter
+How to run:
+(optional): `pip install -r requirements.txt`
+`python main.py`
 """
+
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 import chess
 import chess.engine
 import time
-engine = chess.engine.SimpleEngine.popen_uci('D:\\pobrane\\stockfish.exe')
+
+PATH_TO_ENGINE = chess.engine.SimpleEngine.popen_uci('.\\chess_engine\\stockfish.exe')
+
+
 class ChessGame(TwoPlayerGame):
     def __init__(self, players=None):
         self.board = chess.Board()
         self.players = players
         self.current_player = 1
         self.start_time = time.time()
+
     def possible_moves(self):
         return list(self.board.legal_moves)
+
     def make_move(self, move):
         self.board.push(move)
+
     def unmake_move(self, move):
         self.board.pop()
+
     def win(self):
         return self.board.is_variant_win()
+
     def is_over(self):
         return self.board.is_game_over()
-    def show(self):
-        print(f'Time of execution: {time.time() - self.start_time} seconds')
-        print(self.board)
+
+    def print_termination_reason_if_end(self):
         outcome = self.board.outcome()
         if outcome is not None:
             print(f'Termination: {outcome.termination.name}, result: {outcome.result()}')
-        self.start_time = time.time()
-        info = engine.analyse(self.board, chess.engine.Limit(depth=10))
+
+    def get_analysis_from_engine(self):
+        info = PATH_TO_ENGINE.analyse(self.board, chess.engine.Limit(depth=10))
+        return info
+
+    def print_current_score(self):
+        info = self.get_analysis_from_engine()
         print("Score:", info["score"])
+
+    def print_possible_moves(self):
         moves = list(self.board.legal_moves)
-        print(f'Probably best move: {info["pv"][0]}')
         print(moves)
+
+    def print_last_move_execution_time(self):
+        print(f'Time of execution: {time.time() - self.start_time} seconds')
+        # reset start_time to current time
+        self.start_time = time.time()
+
+    def show(self):
+        self.print_last_move_execution_time()
+
+        print(self.board)
+
+        self.print_termination_reason_if_end()
+        self.print_current_score()
+        self.print_possible_moves()
+
     def scoring(self):
         if self.board.is_checkmate():
+            """
+            That means current player is being checkmated
+            not that current player can check mate an opponent
+            """
             return -100000
-        info = engine.analyse(self.board, chess.engine.Limit(depth=20))
-        multiplier = 1
-        kara = 0
-        #if self.current_player == 1:
-        #    multiplier = 0.3
-        #    #kara = randrange(100, 8000)
+
+        info = self.get_analysis_from_engine()
+
         try:
-            return info["score"].relative.cp * multiplier - kara
+            """
+            Example output from engine:
+            PovScore(Cp(+32), WHITE)
+            Means that WHITE in this situation is in a little better position
+            """
+            return info["score"].relative.cp
+
         except:
-            return (20000 - 2000 * info["score"].relative.moves) * multiplier - kara
+            """
+            Example: PovScore(Mate(+2), WHITE)
+            Means that WHITE can check mate opponent in 2 moves
+            Give more score when check mate is available in less moves
+            """
+            return 20000 - 2000 * info["score"].relative.moves
+
+
 # Start a match (and store the history of moves when it ends)
-ai = Negamax(1, win_score=100000)
-ai2 = Negamax(1)
-#game = ChessGame( [ AI_Player(ai), Human_Player() ] )
-game = ChessGame( [ Human_Player(), AI_Player(ai) ] )
+ai = Negamax(1, win_score=20000)
+#game = ChessGame( [ Human_Player(), AI_Player(ai) ] )
+game = ChessGame( [ AI_Player(ai), AI_Player(ai) ] )
 history = game.play()
